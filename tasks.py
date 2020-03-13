83a1dc12a1953946b002c404b453b4ab8151ccd5
Aleksandr Ternikov <a.ternikov@vezet.ru>
2020-03-13 15:28:55 +0300

    use writelines

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ import datetime as dt
 
 todos_url = "https://json.medrating.org/todos"
 users_url = "https://json.medrating.org/users"
-time_now=dt.datetime.now()
+time_now = dt.datetime.now()
 
 
 users = json.loads(urllib.request.urlopen(users_url).read())
@@ -18,38 +18,38 @@ os.makedirs('tasks', exist_ok=True)
 
 def generate_reports():
     for user in users:
-#    completed_tasks = [x["title"]+'\n' for x in tasks
-#                      if x["userId"] == user["id"] and x["completed"]]
-        completed_tasks = (
-            ''.join(x["title"]+'\n'
+        completed_tasks = [
+            x["title"]+'\n'
             for x in tasks
-            if x["userId"] == user["id"] and x["completed"])
-           )
-        uncompleted_tasks = (
-            ''.join(
-            x["title"]+'\n' if len(x["title"]) < 50 else x["title"][:50]+'...\n'
+            if x["userId"] == user["id"] and x["completed"]
+        ]
+        uncompleted_tasks = [
+            x["title"]+'\n' if len(x["title"]) < 50
+            else x["title"][:50]+'...\n'
             for x in tasks
-            if x["userId"] == user["id"] and not x["completed"])
-            )
-
-
+            if x["userId"] == user["id"] and not x["completed"]
+        ]
 
         if os.path.isfile(f'tasks/{user["username"]}.txt'):
             creation_time = os.path.getctime(f'tasks/{user["username"]}.txt')
-            formatted_time = dt.datetime.fromtimestamp(creation_time).strftime('%Y-%m-%dT%H:%M')
-            os.rename(f'tasks/{user["username"]}.txt', f'tasks/{user["username"]}_{formatted_time}.txt')
+            formatted_time = dt.datetime.fromtimestamp(
+                creation_time).strftime('%Y-%m-%dT%H:%M')
+            os.rename(f'tasks/{user["username"]}.txt',
+                      f'tasks/{user["username"]}_{formatted_time}.txt')
 
         try:
             with open(f'tasks/{user["username"]}.txt', 'w') as f:
-                f.write(f'{user["name"]} <{user["email"]}> {time_now.strftime("%d.%m.%Y %H:%M")}\n')
+                f.write(
+                    f'{user["name"]} <{user["email"]}> {time_now.strftime("%d.%m.%Y %H:%M")}\n')
                 f.write(f'{user["company"]["name"]}\n\n')
                 f.write("Завершённые задачи:\n")
-                f.write(completed_tasks)
+                f.writelines(completed_tasks)
                 f.write("\nОставшиеся задачи:\n")
-                f.write(uncompleted_tasks)
+                f.writelines(uncompleted_tasks)
 
         except IOError:
             print("An IOError has occurred!")
+
 
 if __name__ == '__main__':
     generate_reports()
