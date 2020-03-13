c3777e2d13c4e814715dddd517e3c0cae56829f1
Aleksandr Ternikov <a.ternikov@vezet.ru>
2020-03-13 16:53:08 +0300

    Added exceptions

--- tasks.py
+++ tasks.py
@@ -1,53 +1,64 @@
-import urllib.request
+import datetime as dt
 import json
 import os
-import datetime as dt
+import urllib.request
 
 
 todos_url = "https://json.medrating.org/todos"
 users_url = "https://json.medrating.org/users"
-time_now = dt.datetime.now()
 
+time_now = dt.datetime.now().strftime("%d.%m.%Y %H:%M")
 
-users = json.loads(urllib.request.urlopen(users_url).read())
-tasks = json.loads(urllib.request.urlopen(todos_url).read())
+with urllib.request.urlopen(users_url) as url:
+    users = json.loads(url.read().decode())
 
-
-os.makedirs('tasks', exist_ok=True)
+with urllib.request.urlopen(todos_url) as url:
+    tasks = json.loads(url.read().decode())
 
 
 def generate_reports():
+    try:
+        os.makedirs('tasks', exist_ok=True)
+    except:
+        raise OSError("Can't create destination directory tasks!")
+
+    os.chdir('tasks')
     for user in users:
         completed_tasks = [
-            x["title"]+'\n'
+            x["title"]
             for x in tasks
             if x["userId"] == user["id"] and x["completed"]
         ]
         uncompleted_tasks = [
-            x["title"]+'\n' if len(x["title"]) < 50
-            else x["title"][:50]+'...\n'
+            x["title"] if len(x["title"]) < 50
+            else x["title"][:50]+'...'
             for x in tasks
             if x["userId"] == user["id"] and not x["completed"]
         ]
 
-        if os.path.isfile(f'tasks/{user["username"]}.txt'):
-            creation_time = os.path.getctime(f'tasks/{user["username"]}.txt')
+        if os.path.isfile(f'{user["username"]}.txt'):
+            creation_time = os.path.getctime(f'{user["username"]}.txt')
             formatted_time = dt.datetime.fromtimestamp(
                 creation_time).strftime('%Y-%m-%dT%H:%M')
-            os.rename(f'tasks/{user["username"]}.txt',
-                      f'tasks/{user["username"]}_{formatted_time}.txt')
+            os.rename(f'{user["username"]}.txt',
+                      f'{user["username"]}_{formatted_time}.txt')
 
         try:
-            with open(f'tasks/{user["username"]}.txt', 'w') as f:
+            with open(f'{user["username"]}.txt', 'w') as f:
+                newline = '\n'
                 f.write(
-                    f'{user["name"]} <{user["email"]}> {time_now.strftime("%d.%m.%Y %H:%M")}\n')
-                f.write(f'{user["company"]["name"]}\n\n')
-                f.write("Завершённые задачи:\n")
-                f.writelines(completed_tasks)
-                f.write("\nОставшиеся задачи:\n")
-                f.writelines(uncompleted_tasks)
+                    f"""{user["name"]} <{user["email"]}> {time_now}
+{user["company"]["name"]}
 
+Завершённые задачи:
+{newline.join(completed_tasks)}
+
+Оставшиеся задачи:
+{newline.join(uncompleted_tasks)}"""
+                )
         except IOError:
+            os.rename(f'{user["username"]}_{formatted_time}.txt', f'{user["username"]}.txt'
+                      )
             print("An IOError has occurred!")
 
 
