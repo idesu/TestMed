3dbac1cdeabb6edd791e99a37ff9e15e2e67db49
Alexander Ternikov <herr.potato@pm.me>
2020-03-14 21:54:25 +0300

    Refactored, use request lib, added requirements and prettify.

--- tasks.py
+++ tasks.py
@@ -1,30 +1,28 @@
 import datetime as dt
 import os
 import ssl
-import requests
 from collections import defaultdict
 
-api_base = "https://json.medrating.org"
-api_user = "{}/users".format(api_base)
-api_todos = "{}/todos".format(api_base)
-# Certificate verify hack for MacOS with embedded version of OpenSSL,
-# which does not use the system certificate store by default
-# This is the Way:
-# /Applications/Python\ 3.7/Install Certificates.command
-context = ssl._create_unverified_context()
+import requests
 
-completed = defaultdict(list)
-uncompleted = defaultdict(list)
-uncompleted_title_max_length = 50
-out_dir='tasks'
+API_BASE = "https://json.medrating.org"
+API_USERS = f"{API_BASE}/users"
+API_TODOS = f"{API_BASE}/todos"
+MAX_TASK_TITLE_LEN = 50
+OUT_DIR = 'tasks'
 
-def get_data(api_user, api_todos):
-    users = requests.get(api_user).json()
-    todos = requests.get(api_todos).json()
-    return users, todos
 
-def make_report_for_all_users(users, tasks):
-    for task in tasks:
+def get_data(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.json()
+
+
+def make_report_for_all_users(todos):
+    completed = defaultdict(list)
+    uncompleted = defaultdict(list)
+    for task in todos:
         if task['completed']:
             completed[task['userId']].append(task['title'])
         else:
@@ -32,66 +30,92 @@ def make_report_for_all_users(users, tasks):
     return completed, uncompleted
 
 
-def generate_user_reports(max_length = 50):
-    users, tasks = get_data(api_user, api_todos)
-    completed_tasks, uncompleted_tasks = make_report_for_all_users(users, tasks)
-
-    try:
-        os.makedirs('tasks', exist_ok=True)
-    except:
-        raise OSError("Can't create destination directory tasks!")
-    os.chdir(out_dir)
-
-    time_now = dt.datetime.now().strftime("%d.%m.%Y %H:%M")
-
-    for user in users:
-        user_key = user["id"]
-        username = user["username"]
-
-        if len(completed_tasks[user_key]) and len(uncompleted_tasks[user_key]) == 0:
-            continue
-
-        uncompleted_tasks_formatted = [
-            (task[:max_length] + '...') 
-            if len(task) > max_length
-            else task 
-            for task in uncompleted_tasks[user_key]
-        ]
-
-        if os.path.isfile(f'{username}.txt'):
-            creation_time = os.path.getctime(f'{username}.txt')
-            formatted_ctime = dt.datetime.fromtimestamp(
-                creation_time).strftime('%Y-%m-%dT%H:%M')
-            os.rename(f'{username}.txt',
-                      f'{username}_{formatted_ctime}.txt')
-        try:
-            with open(f'{username}.txt', 'w') as f:
-                newline = '\n'
-                f.write(
-                    f"""{user["name"]} <{user["email"]}> {time_now}
+def format_report(user, completed, uncompleted):
+    def format_title(title):
+        if len(title) <= MAX_TASK_TITLE_LEN:
+            return title
+        return title[:MAX_TASK_TITLE_LEN] + '...'
+    
+    completed = [format_title(title) for title in completed]
+    uncompleted = [format_title(title) for title in uncompleted]
+    now_time = dt.datetime.now().strftime("%d.%m.%Y %H:%M")
+    newline = '\n'
+    
+    return (
+f"""{user["name"]} <{user["email"]}> {now_time}
 {user["company"]["name"]}
 
 Завершённые задачи:
-{newline.join(completed_tasks[user_key])}
-
+{newline.join(completed)}
+    
 Оставшиеся задачи:
-{newline.join(uncompleted_tasks_formatted)}"""
-                )
-        except:
-            try:
-                os.remove(f'{username}.txt')
-                print(
-                    f'Cant write to {username}.txt new file removed successfully')
-            except OSError as e:
-                if e.errno == ENOENT:
-                    # suppress "FileNotFoundError"
-                    pass
-                else:
-                    raise
-            os.rename(f'{username}_{formatted_ctime}.txt',
-                      f'{username}.txt')
-            raise
+{newline.join(uncompleted)}
+"""
+)
+
+def save_report_to_file(user, completed, uncompleted):
+    if not completed and not uncompleted:
+        return
+
+    report_name = f"{OUT_DIR}/{user['username']}.txt"
+
+    try:
+        with open(report_name, "r") as f:
+            prev_date = f.readline().rstrip().rsplit(None, 2)[-2:]
+    except (OSError, IOError) as e:
+        prev_date = None
+    
+    with open(f"{report_name}.new", "w") as f:
+        f.write(format_report(user, completed, uncompleted))
+
+    if prev_date:
+        date_fname = dt.datetime.strptime(
+            prev_date[0] + " " + prev_date[1],
+            "%d.%m.%Y %H:%M"
+        ).strftime("%Y-%m-%dT%H:%M")
+        os.rename(
+            report_name,
+            f"{OUT_DIR}/{user['username']}_{date_fname}.txt"
+        )
+    os.rename(f"{report_name}.new", report_name)
+
+
+
+def full_report(users, all_users_completed, all_users_uncompleted):
+    for user in users:
+        save_report_to_file(
+            user,
+            all_users_completed.get(user['id'], []),
+            all_users_uncompleted.get(user['id'], [])
+        )
+
+    all_users_ids = set([user['id'] for user in users])
+    completed_ids = set(all_users_completed.keys())
+    uncompleted_ids = set(all_users_uncompleted.keys())
+
+    if all_users_ids == completed_ids.union(uncompleted_ids):
+        return
+
+    print("== Extra ==")
+    print("Users with no tasks: ")
+    print(", ".join(list(all_users_ids.difference(
+        completed_ids.union(uncompleted_ids)))))
+    print("User ids from task with no user description: ")
+    print(", ".join(list(completed_ids.union(uncompleted_ids).difference(all_users_ids))))
+
+
+def main():
+    users = get_data(API_USERS)
+    todos = get_data(API_TODOS)
+    all_users_completed, all_users_uncompleted = make_report_for_all_users(todos)
+
+    try:
+        os.makedirs(OUT_DIR, exist_ok=True)
+    except:
+        raise OSError("Can't create destination directory tasks!")
+
+    full_report(users, all_users_completed, all_users_uncompleted)
 
 
 if __name__ == '__main__':
-    generate_user_reports(max_length=uncompleted_title_max_length)
+    main()
