27dc2afbd9a3e21a77735721d7e4f37c9d9de77d
Alexander Ternikov <herr.potato@pm.me>
2020-03-14 18:36:16 +0300

    Ready to refactoring

--- tasks.py
+++ tasks.py
@@ -1,48 +1,62 @@
 import datetime as dt
-import json
 import os
 import ssl
-import urllib.request
+import requests
+from collections import defaultdict
 
-todos_url = "https://json.medrating.org/todos"
-users_url = "https://json.medrating.org/users"
+api_base = "https://json.medrating.org"
+api_user = "{}/users".format(api_base)
+api_todos = "{}/todos".format(api_base)
 # Certificate verify hack for MacOS with embedded version of OpenSSL,
 # which does not use the system certificate store by default
 # This is the Way:
 # /Applications/Python\ 3.7/Install Certificates.command
 context = ssl._create_unverified_context()
 
+completed = defaultdict(list)
+uncompleted = defaultdict(list)
+uncompleted_title_max_length = 50
+out_dir='tasks'
 
-def generate_reports():
+def get_data(api_user, api_todos):
+    users = requests.get(api_user).json()
+    todos = requests.get(api_todos).json()
+    return users, todos
+
+def make_report_for_all_users(users, tasks):
+    for task in tasks:
+        if task['completed']:
+            completed[task['userId']].append(task['title'])
+        else:
+            uncompleted[task['userId']].append(task['title'])
+    return completed, uncompleted
+
+
+def generate_user_reports(max_length = 50):
+    users, tasks = get_data(api_user, api_todos)
+    completed_tasks, uncompleted_tasks = make_report_for_all_users(users, tasks)
+
+    try:
+        os.makedirs('tasks', exist_ok=True)
+    except:
+        raise OSError("Can't create destination directory tasks!")
+    os.chdir(out_dir)
+
     time_now = dt.datetime.now().strftime("%d.%m.%Y %H:%M")
 
-    with urllib.request.urlopen(users_url, context=context) as url:
-        users = json.loads(url.read().decode())
-
-    with urllib.request.urlopen(todos_url, context=context) as url:
-        tasks = json.loads(url.read().decode())
-
-    os.makedirs('tasks', exist_ok=True)
-    os.chdir('tasks')
-
     for user in users:
-        completed_tasks = [
-            task["title"]
-            for task in tasks
-            if task["userId"] == user["id"] and task["completed"]
-        ]
-        task_title_max_length = 50
-        uncompleted_tasks = [
-            task["title"] if len(task["title"]) < task_title_max_length
-            else task["title"][:task_title_max_length]+'...'
-            for task in tasks
-            if task["userId"] == user["id"] and not task["completed"]
-        ]
+        user_key = user["id"]
+        username = user["username"]
 
-        if len(uncompleted_tasks) and len(completed_tasks) == 0:
+        if len(completed_tasks[user_key]) and len(uncompleted_tasks[user_key]) == 0:
             continue
 
-        username = user["username"]
+        uncompleted_tasks_formatted = [
+            (task[:max_length] + '...') 
+            if len(task) > max_length
+            else task 
+            for task in uncompleted_tasks[user_key]
+        ]
 
         if os.path.isfile(f'{username}.txt'):
             creation_time = os.path.getctime(f'{username}.txt')
@@ -58,10 +72,10 @@ def generate_reports():
 {user["company"]["name"]}
 
 Завершённые задачи:
-{newline.join(completed_tasks)}
+{newline.join(completed_tasks[user_key])}
 
 Оставшиеся задачи:
-{newline.join(uncompleted_tasks)}"""
+{newline.join(uncompleted_tasks_formatted)}"""
                 )
         except:
             try:
@@ -76,7 +90,8 @@ def generate_reports():
                     raise
             os.rename(f'{username}_{formatted_ctime}.txt',
                       f'{username}.txt')
+            raise
 
 
 if __name__ == '__main__':
-    generate_reports()
+    generate_user_reports(max_length=uncompleted_title_max_length)
